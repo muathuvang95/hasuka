1e62be2bef67b31b9270d34098f4d9309f1c546e
quacheck <qqngoc2988@gmail.com>
2018-11-26 15:21:27 +0700

    Fix script carousel

--- js/script.js
+++ js/script.js
@@ -3,19 +3,17 @@ jQuery(document).ready(function($){
 		loop:true,
 		margin:10,
 		responsiveClass:true,
+		nav:true,
+		dots:false,
 		responsive:{
 			0:{
-				items:1,
-				nav:true
+				items:1
 			},
 			600:{
-				items:1,
-				nav:true
+				items:1
 			},
 			768:{
-				items:3,
-				nav:true,
-				loop:false
+				items:3
 			}
 		}
 	});
@@ -23,19 +21,17 @@ jQuery(document).ready(function($){
 		loop:true,
 		margin:10,
 		responsiveClass:true,
+		nav:true,
+		dots:false,
 		responsive:{
 			0:{
-				items:1,
-				nav:true
+				items:1
 			},
 			600:{
-				items:1,
-				nav:true
+				items:1
 			},
 			768:{
-				items:2,
-				nav:true,
-				loop:false
+				items:2
 			}
 		}
 	});
